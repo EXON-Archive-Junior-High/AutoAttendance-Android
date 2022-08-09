b0e3d35631d67b7382b5719276818222b902b1c4
시혁 박 <exon.indiv@gmail.com>
2022-08-09 21:23:48 +0900

    Update: 서비스 Socket

--- app/src/main/java/com/example/chat/socketService.java
+++ app/src/main/java/com/example/chat/socketService.java
@@ -1,39 +1,24 @@
 package com.example.chat;
 
+import android.app.Service;
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.os.Bundle;
 import android.os.Handler;
+import android.os.IBinder;
 import android.os.Message;
 import android.util.Log;
-import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
-import java.util.logging.LogRecord;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
+public class socketService  extends Service {
+    int mStartMode;       // indicates how to behave if the service is killed
+    IBinder mBinder;      // interface for clients that bind
+    boolean mAllowRebind; // indicates whether onRebind should be used
     public Socket socket = null;
     private String host = "192.168.35.82";
     private int port = 6666;
@@ -42,91 +27,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private final int LOGIN_ERROR = 1;
 
     private SendMessageHandler mHandler = null;
-
-    private Button mLoginBtn = null;
-    private EditText mIdEdit = null;
-    private EditText mPasswordEdit = null;
-
     private boolean isConnected = false;
-
+    private String mId = null;
+    private String mPwd = null;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        // ClientThread thread = new ClientThread();
-        // thread.start();
-
-        mLoginBtn = (Button)findViewById(R.id.button);
-        mIdEdit = (EditText)findViewById(R.id.id_edit);
-        mPasswordEdit = (EditText)findViewById(R.id.password_edit);
-
-        mLoginBtn.setOnClickListener(this);
+    public void onCreate() {
+        // 서비스에서 가장 먼저 호출됨(최초에 한번만)
+    }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // 서비스가 호출될 때마다 실행
         mHandler = new SendMessageHandler();
+        mId = intent.getStringExtra("id");
+        mPwd = intent.getStringExtra("pwd");
 
-        // just test
-        mIdEdit.setText("1");
-        mPasswordEdit.setText("1");
+        Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|intent.FLAG_ACTIVITY_SINGLE_TOP|Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        mainIntent.putExtra("msg","안녕하세욯 !");
+        startActivity(mainIntent);
 
-        // Start Socket Service
-        Intent intent = new Intent( getApplicationContext(),//현재제어권자
-                socketService.class); // 이동할 컴포넌트
-        intent.putExtra("id", mIdEdit.getText().toString());
-        intent.putExtra("pwd", mPasswordEdit.getText().toString());
-        startService(intent); // 서비스 시작
-
-    }
-
-    @Override
-    protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-        if( intent != null) {
-            String Msg = intent.getStringExtra("msg");
-            Toast.makeText(MainActivity.this, "Service 에서 전달된 Msg = ." + Msg, Toast.LENGTH_SHORT).show();
-        }
-
+        return mStartMode;
     }
     @Override
-    protected void onPause(){
-        super.onPause();
-        // Thread나 소켓 recv를 잠시멈춤..
+    public IBinder onBind(Intent intent) {
+        // A client is binding to the service with bindService()
+        return mBinder;
     }
-
     @Override
-    protected void onResume(){
-        super.onResume();
-        // Thread나 소켓 recv 를 다시 시작.
+    public boolean onUnbind(Intent intent) {
+        // All clients have unbound with unbindService()
+        return mAllowRebind;
     }
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onRebind(Intent intent) {
+        // A client is binding to the service with bindService(),
+        // after onUnbind() has already been called
     }
-
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onClick(View v) {
-        Log.d("Login", "log");
-        if (socket == null) {
-            ClientThread thread = new ClientThread();
-            thread.start();
-        }
+    public void onDestroy() {
+        // The service is no longer used and is being destroyed
     }
 
     public class SendMessageHandler extends Handler {
@@ -135,21 +74,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             switch (msg.what) {
                 case LOGIN_SUCCESS:
-                    Toast.makeText(MainActivity.this, "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(MainActivity.this,ChatActivity.class);
-                    startActivity(intent);//액티비티 띄우기
+                    //Toast.makeText(MainActivity.this, "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show();
+                    //Intent intent = new Intent(MainActivity.this,ChatActivity.class);
+                    //startActivity(intent);//액티비티 띄우기
                     break;
                 case LOGIN_ERROR:
-                    Toast.makeText(MainActivity.this, msg.getData().getString("msg"), Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(MainActivity.this, msg.getData().getString("msg"), Toast.LENGTH_SHORT).show();
                     break;
             }
         }
     }
-
-    public Socket getSocket() {
-        return socket;
-    }
-
 
     class ClientThread extends Thread {
         @Override
@@ -161,7 +95,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                     DataOutputStream outstream = new DataOutputStream(socket.getOutputStream());
                     String msg = "";
-                    msg = "Login/" + mIdEdit.getText().toString() + "/" + mPasswordEdit.getText().toString();
+                    msg = "Login/" + mId + "/" + mPwd;
                     outstream.writeUTF(msg);
                     // Log.d("Message", msg.getBytes("UTF-8").toString());
                     outstream.flush();
